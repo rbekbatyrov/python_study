5528592b06be8028d2bf377db406fb8bfe6c4f67
rbekbatyrov <69893694+rbekbatyrov@users.noreply.github.com>
2021-11-09 18:08:25 +0600

    Create bicycles.py

---
+++ bicycles.py
@@ -0,0 +1,33 @@
+#create bicycle and print the first element
+bicycle=[1, 2, 'a']
+print(bicycle)
+print(bicycle[0])
+print('---')
+
+#add one more element to the end
+bicycle.append('b')
+print(bicycle)
+print('---')
+
+#insert element to the begining
+bicycle.insert(0, '3')
+print(bicycle)
+print('---')
+
+#delete the first element
+del bicycle[0]
+print(bicycle)
+print('---')
+
+#delete the last element and the seconf element
+bicycle.pop()
+print(bicycle)
+print('---')
+bicycle.pop(1)
+print(bicycle)
+print('---')
+
+#find and delete the 'a' element
+bicycle.remove('a')
+print(bicycle)
+print('---')
